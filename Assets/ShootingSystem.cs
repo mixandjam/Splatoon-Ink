c34062c024a12510af0a06bdf317d8b668cffe66
André Cardoso <amc.acardoso@gmail.com>
2021-02-22 16:03:32 +0100

    animation fixes

--- Assets/ShootingSystem.cs
+++ Assets/ShootingSystem.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Cinemachine;
 
 public class ShootingSystem : MonoBehaviour
 {
 
     [SerializeField] ParticleSystem inkParticle;
     [SerializeField] ParticleSystem extraParticle;
-    [SerializeField] Transform[] ikHandlers;
-    [SerializeField] Transform splatGun;
+    [SerializeField] Transform parentController;
     [SerializeField] Transform splatGunNozzle;
+    [SerializeField] CinemachineFreeLook freeLookCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +22,26 @@ public class ShootingSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        GetComponent<MovementInput>().blockRotationPlayer= Input.GetMouseButton(0);
+        Vector3 angle = parentController.localEulerAngles;
+        GetComponent<MovementInput>().blockRotationPlayer = Input.GetMouseButton(0);
+        bool pressing = GetComponent<MovementInput>().blockRotationPlayer;
         if (Input.GetMouseButton(0))
         {
-            foreach (Transform handler in ikHandlers)
+            if (!DOTween.IsTweening(parentController))
             {
-                if (!DOTween.IsTweening(handler))
-                {
-                    handler.DOComplete();
-                    handler.DOPunchPosition(-transform.forward / 13, .15f, 10, 1);
-                }
+                parentController.DOComplete();
+                Vector3 forward = -parentController.forward;
+                Vector3 localPos = parentController.localPosition;
+                parentController.DOLocalMove(localPos - new Vector3(0, 0, .1f), .03f)
+                    .OnComplete(() => parentController.DOLocalMove(localPos,.1f).SetEase(Ease.OutSine));
             }
 
             if (!DOTween.IsTweening(splatGunNozzle))
             {
                 splatGunNozzle.DOComplete();
-                splatGunNozzle.DOPunchScale(new Vector3(0,1,1) / 3, .15f, 10, 1);
+                splatGunNozzle.DOPunchScale(new Vector3(0,1,1) / 1.5f, .15f, 10, 1);
             }
+
             GetComponent<MovementInput>().RotateToCamera(transform);
         }
         if (Input.GetMouseButtonDown(0))
@@ -51,5 +54,14 @@ public class ShootingSystem : MonoBehaviour
             inkParticle.Stop();
             extraParticle.Stop();
         }
+
+        parentController.localEulerAngles 
+            = new Vector3(Mathf.LerpAngle(parentController.localEulerAngles.x, pressing ? RemapCamera(freeLookCamera.m_YAxis.Value, 0, 1, -25, 25) : 0,.3f), angle.y, angle.z);
+    }
+
+
+    float RemapCamera(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
